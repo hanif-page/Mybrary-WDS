cc31cf54478e18c8290b89cc5bed244e9e87992e
hanif-page <amrhanif15@gmail.com>
2022-02-27 13:44:48 +0700

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const router = express.Router()
+const Author = require("../models/author")
+
+// All Authors Route
+router.get('/', async (req, res) => {
+    let searchOptions = {}  
+    if(req.query.name != null && req.query.name !== "") {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+
+    try {
+        // get all authors data, where {} mean is we want all the data inside without any condition
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch {
+        // back to home page
+        res.redirect("/")
+    }
+})
+
+// New Authors Route 
+router.get("/new", (req, res) => {
+    res.render("authors/new", { author: new Author()})
+})
+
+// Create Authors Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name 
+    })
+
+    // NOTE : all mongodb and mongoose action are asynchrounous, so it's a good thing to use async await and try catch method.
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch(err) {
+        res.render('authors/new', {
+            author: author,
+            errMsg: `Error creating author`
+        })
+    }
+
+})
+ 
+module.exports = router
+
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ if(process.env.NODE_ENV !== "production")
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require("body-parser")
+
 const indexRouter = require("./routes/index")
+const authorRouter = require("./routes/authors")
 
 // default setting
 app.set('view engine', 'ejs')
@@ -14,16 +17,23 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection 
 db.on('error', error => console.error(error))
-db.once('open', () => console.log("connecter to mongoose"))
+db.once('open', () => console.log("connected to mongoose"))
 
 
 // index route
 app.use('/', indexRouter)
+app.use("/authors", authorRouter)
+
+// Handling non matching request from the client
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Page Not Found</h1>")
+})
 
 const port = process.env.PORT || 3000 
 app.listen(port, () => console.log(`server running on port ${port}`))
